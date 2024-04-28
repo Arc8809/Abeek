28a78f6e400ffc529e8705ecd8492f8fe5a56868
Kevin Wang <kw4006@g.rit.edu>
2024-04-27 20:26:40 -0400

    Added css definitions for content pages

--- pages/intro-unix.php
+++ pages/intro-unix.php
@@ -15,28 +15,18 @@
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
     </div><br>
-    <div>
-        TESTTT!!!!!!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-    </div><br><div>
-        TESTTT!!!!!!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-    </div><br><div>
-        TESTTT!!!!!!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-    </div><br>
-    <div>
-        TESTTT!!!!!!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-    </div><br><div>
-        TESTTT!!!!!!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-    </div><br><div>
+
+    <div> 
+    <h2>cd Command</h2>
+
+        <div class="block">
+            <div class="command">
+                <p>$ command line stuff </p>
+            </div>
+        </div> 
+    </div>
+
+    <div class="page_text">
         TESTTT!!!!!!
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
