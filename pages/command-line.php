91ded312d71b9e969337881ea064de17f407be31
Kevin Wang <kw4006@g.rit.edu>
2024-04-27 21:29:20 -0400

    Added some content to command-line.php for testing

--- pages/command-line.php
+++ pages/command-line.php
@@ -8,38 +8,62 @@
 
 ?>
 <main class="content_body">
-     
-     <div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br>
-     <div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br><div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br><div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br>
-     <div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br><div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br><div>
-         TESTTT!!!!!!
-         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-     </div><br>
+
+    <div>
+        <h1>Command Line Basics</h1> 
+
+        <p>Here are some basic commands to introduce to the Unix environment to you.</p>
+
+    </div>
+
+    <h2>Basic File Manipulation Commands<h2>
+    <div>
+        <h3>mkdir command</h3>
+        <div class="block">
+            <div class="command">
+                <p>$ mkdir example</p>
+            </div>
+        </div> 
+
+
+        <div class="page_text">
+            <p>The mkdir command takes in a parameter and creates a directory with the name of the given parameter. In the 
+                    example provided above, a directory with the name "example" is created. This directory will be created on the relative
+                    path to your current location on the system. To specify the creation of a directory at an absolute path, you can 
+                    provide an absolute path instead, for example, "mkdir /home/student/example" creates the example directory on 
+                    the path "/home/student".
+            </p>
+        </div>
+
+        <h3>cd command</h3>
+        <div class="block">
+            <div class="command">
+                <p>$ cd example</p>
+            </div>
+        </div> 
+
+
+        <div class="page_text">
+            <p>The cd command takes in a parameter and changes the current directory to the given parameter provided
+                that the directory exists on the current path. To change to a directory on an absolute path, you can provide 
+                in an absolute path instead of the directory name.
+            </p>
+        </div>
+
+        <div class="block">
+            <div class="command">
+                <p>$ cd /home/student/example</p>
+            </div>
+        </div> 
+
+        <div class="page_text">
+            <p>In the example above, we change directory to the example student directory that is on the path "/home/student".
+            </p>
+        </div>
+
+    </div>
+
+
  </main>
  
 <?php 
