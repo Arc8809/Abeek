24c65aa000b3953b417f465528e3dd3baf6b2b35
Anderson C <Andercard4212@hotmail.com>
2024-04-28 23:23:33 -0400

    interctive

--- pages/interactive-examples.php
+++ pages/interactive-examples.php
@@ -9,24 +9,10 @@
 ?>
 
 <main class="content_body">
-<div class="console">
-    <div><span class="prompt">$</span> ls</div>
-    <div class="output">Documents  Downloads  Pictures  Music</div>
-    <div><span class="prompt">$</span> pwd</div>
-    <div class="output">/home/user</div>
-    <div><span class="prompt">$</span> cd Documents</div>
-    <div><span class="prompt">$</span> ls</div>
-    <div class="output">projects  notes.txt</div>
-    <div><span class="prompt">$</span> cat notes.txt</div>
-    <div class="output">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</div>
-    <div><span class="prompt">$</span> mkdir new_project</div>
-    <div><span class="prompt">$</span> cd new_project</div>
-    <div><span class="prompt">$</span> touch script.sh</div>
-    <div><span class="prompt">$</span> nano script.sh</div>
-</div>
-
-
-
+<div>
+    <h1>ERROR 404 PAGE NOT FOUND</h1>
+    <p>Due to to much and ambition and not enough time this page is non-existent</p>   
+</div>  
 </main>
 
 <?php 
