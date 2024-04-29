8d6d79939855f63b785694b3515ebc8fb37cb2ee
Anderson C <Andercard4212@hotmail.com>
2024-04-28 23:16:27 -0400

    interactive page

--- pages/interactive-examples.php
+++ pages/interactive-examples.php
@@ -9,7 +9,21 @@
 ?>
 
 <main class="content_body">
-     
+<div class="console">
+    <div><span class="prompt">$</span> ls</div>
+    <div class="output">Documents  Downloads  Pictures  Music</div>
+    <div><span class="prompt">$</span> pwd</div>
+    <div class="output">/home/user</div>
+    <div><span class="prompt">$</span> cd Documents</div>
+    <div><span class="prompt">$</span> ls</div>
+    <div class="output">projects  notes.txt</div>
+    <div><span class="prompt">$</span> cat notes.txt</div>
+    <div class="output">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</div>
+    <div><span class="prompt">$</span> mkdir new_project</div>
+    <div><span class="prompt">$</span> cd new_project</div>
+    <div><span class="prompt">$</span> touch script.sh</div>
+    <div><span class="prompt">$</span> nano script.sh</div>
+</div>
 
 
 
