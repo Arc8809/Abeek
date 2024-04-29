570b9607f22796ade4d03f8b38a7bfba8018c62d
Anderson C <Andercard4212@hotmail.com>
2024-04-28 23:59:08 -0400

    last commit

--- pages/certifications.php
+++ pages/certifications.php
@@ -7,38 +7,20 @@
     include($path."assets/inc/nav.php");
 
 ?>
+<main class="content_body">
 <div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br>
-<div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br><div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br><div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br>
-<div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br><div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br><div>
-    TESTTT!!!!!!
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam adipisci ipsa exercitationem iure. Laboriosam reprehenderit commodi amet, mollitia id vitae est debitis earum sint aut doloremque sapiente aliquid. Nam, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat mollitia ut consequatur quos atque doloremque alias assumenda, totam, veniam perspiciatis hic velit quasi, accusamus repudiandae itaque magnam fugit molestias.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iure vel omnis minima consectetur totam velit quisquam, id reiciendis dignissimos nobis et sequi minus sint tempora laboriosam nemo possimus quos?
-</div><br>
+   
+    <h1>Abeek Tutorials</h1>
+    <p>The Future of Learning</p>
+    <h2>This Certification is presented to</h2>
+    <h1>YOU</h1>
+    <p>For succesfully completing Abeek Tutorials</p>
+    <h3>Through Trials and tribulations you have successfully completed our rough course list</h3>
+    
 
+
+</div>
+</main>
 <?php 
     include($path."assets/inc/footer.php");
 ?>
